fcdf4efaf55696967a9c8373193ad2f103c06ba8
nicolasserralha <nicolas.serralha@hotmail.com>
2018-05-16 09:30:40 -0300

    criação das classes entities e relacionamentos

---
+++ projeto/src/main/java/br/com/stadlab/projeto/entity/Estabelecimento.java
@@ -0,0 +1,65 @@
+package br.com.stadlab.projeto.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_estabelecimento")
+public class Estabelecimento {
+	
+	@Id
+	@SequenceGenerator(name="Estabelecimento", sequenceName="sq_tb_estabelecimento",allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_estabelecimento")
+	private Integer id;
+	
+	@Column(name="nome_estabelecimento", length=25, nullable=false)
+	private String nome;
+
+	@JoinColumn(name = "id_tp_estabelecimento")
+	@ManyToOne
+	private TipoEstabelecimento tipo;
+	
+	public Estabelecimento() {
+		super();
+	}
+
+	public Estabelecimento(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public TipoEstabelecimento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoEstabelecimento tipo) {
+		this.tipo = tipo;
+	}
+
+}
+ 
---
+++ projeto/src/main/java/br/com/stadlab/projeto/entity/TipoEstabelecimento.java
@@ -0,0 +1,66 @@
+package br.com.stadlab.projeto.entity;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_tp_estabelecimento")
+public class TipoEstabelecimento {
+	
+	
+	public TipoEstabelecimento() {
+		super();
+	}
+
+	public TipoEstabelecimento(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+		@Id
+	@SequenceGenerator(name="tipoEstabelecimento", sequenceName="sq_tb_tp_estabelecimento",allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_tp_estabelecimento")
+	private Integer id;
+	
+	@Column(name="nm_tp_estabelecimento", length=25, nullable=false)
+	private String nome;
+	
+	@OneToMany(mappedBy = "tipo")
+	private Collection<Estabelecimento> estabelecimentos;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Collection<Estabelecimento> getEstabelecimentos() {
+		return estabelecimentos;
+	}
+
+	public void setEstabelecimentos(Collection<Estabelecimento> estabelecimentos) {
+		this.estabelecimentos = estabelecimentos;
+	}
+
+}
+ 
